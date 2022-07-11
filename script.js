f223214dfe2b40aa03332c9591869c8e5c55f50a
vinaysaraswat33 <107617123+vinaysaraswat33@users.noreply.github.com>
2022-07-11 21:57:21 +0530

    mywebsite

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction()
+{
+    
+    document.getElementById("1").innerHTML = "Vinay saraswat";
+    document.getElementById("2").innerHTML = "201550150";
+
+    document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+    document.getElementById("4").innerHTML = "11-07-2022  ";
+}
+
+
+
+
+
+
+
+        
+       
